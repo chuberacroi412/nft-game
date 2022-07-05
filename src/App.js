96eac1791f9edc493918e75ca70e3ae2b82b7823
chuberacroi412 <lamanhtuan1299@gmail.com>
2022-07-05 23:27:04 +0700

    chuẩn hóa code và thư viện, cập nhật giao diện, fix bug

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import {useDispatch, useSelector} from 'react-redux'
-import {connect} from './redux/blockchain/blockchainAction'
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { connect } from "./redux/blockchain/blockchainActions";
 import { fetchData } from "./redux/data/dataActions";
-import { useEffect } from 'react';
 import * as s from "./styles/globalStyles";
 import LipRenderer from "./components/lipRenderer";
 import _color from "./assets/images/bg/_color.png";
 
 function App() {
-  const dispatch = useDispatch()
-  const blockchain = useSelector((state) => state.blockchain)
+  const dispatch = useDispatch();
+  const blockchain = useSelector((state) => state.blockchain);
   const data = useSelector((state) => state.data);
+  const [power, setPower] = useState(0);
   const [loading, setLoading] = useState(false);
+
 
   const mintNFT = (_account, _name) => {
     setLoading(true);
@@ -30,24 +31,63 @@ function App() {
         setLoading(false);
         console.log(receipt);
         dispatch(fetchData(blockchain.account));
+
+        loadPower(data)
+        console.log(power)
       });
   };
 
-  console.table(blockchain)
+  const levelUpLip = (_account, _id) => {
+    setLoading(true);
+    blockchain.lipToken.methods
+      .levelUp(_id)
+      .send({
+        from: _account,
+      })
+      .once("error", (err) => {
+        setLoading(false);
+        console.log(err);
+      })
+      .then((receipt) => {
+        setLoading(false);
+        console.log(receipt);
+        dispatch(fetchData(blockchain.account));
+      });
+  };
+
+  function loadPower() {
+    
+    if(data) {
+      let temp = 0;
+      data.allOwnerLips.forEach(lip => {
+        temp += +(parseInt(lip.rarity) + parseInt(lip.level)*5);
+      });
+      setPower(temp);
+    }
+
+    
+  }
 
   useEffect(() => {
     if (blockchain.account != "" && blockchain.lipToken != null) {
       dispatch(fetchData(blockchain.account));
+      loadPower()
     }
+
   }, [blockchain.lipToken]);
+
+  useEffect(() => {
+    loadPower()
+
+  }, []);
 
   return (
     <s.Screen image={_color}>
       {blockchain.account === "" || blockchain.lipToken === null ? (
-      <s.Container flex={1} ai={"center"} jc={"center"} style={{backgroundColor: "green"}}>
-        <s.TextTitle>Connect to the game</s.TextTitle>
-        <s.SpacerSmall />
-        <button
+        <s.Container flex={1} ai={"center"} jc={"center"}>
+          <s.TextTitle>Connect to the game</s.TextTitle>
+          <s.SpacerSmall />
+          <button
             onClick={(e) => {
               e.preventDefault();
               dispatch(connect());
@@ -55,11 +95,24 @@ function App() {
           >
             CONNECT
           </button>
-      </s.Container>
+          <s.SpacerXSmall />
+          {blockchain.errorMsg != "" ? (
+            <s.TextDescription>{blockchain.errorMsg}</s.TextDescription>
+          ) : null}
+        </s.Container>
       ) : (
         <s.Container ai={"center"} style={{ padding: "24px" }}>
           <s.TextTitle>Welcome to the game</s.TextTitle>
+          <s.TextTitle>Your power: {power} </s.TextTitle>
           <s.SpacerSmall />
+          <button
+            onClick={(e) => {
+              alert('function not implement yet')
+            }}
+          >
+            FIND GAME
+          </button>
+          <s.SpacerXSmall />
           <button
             disabled={loading ? 1 : 0}
             onClick={(e) => {
